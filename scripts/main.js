6e6421a1629af323fbf58eeb030c6156b572b781
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2020-08-19 22:18:11 -0400

    started building js calculations

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+function add (a,b) {return a+b};
+
+function subtract (a,b) {return a-b};
+
+function multiply (a,b) {return a*b};
+
+function divide (a,b) {return a/b};
+
+function modulo (a,b) {return a%b};
+
+function calculate (a, op, b) { return a + op + b};
+
+document.addEventListener('click', (e) => {
+    const input = document.querySelector(`.value[id='${e.target.id}']`).innerHTML;
+    console.log(input);
+
+    // take in multiple button clicks as array elements, then join to a string, then convert to an integer
+
+    // the following variable assignment doesn't register because it follows the `input` variable
+    // try setting these as `function input1()`, `function operand()`, and `function input2()'
+    const operand = document.querySelector(`.operator[id='${e.target.id}']`).innerHTML;
+    console.log(operand);
+
+    // tain in multiple button clicks as array elements, thenn join to a string, then conver to an integer
+
+    // create a `calculate()` function so that `input`, `operand` and `input2` are put into the calculuation
+    calculate(a, op, b);
+});
+
